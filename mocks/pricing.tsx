8896e4b7298e5ce1d0c46a23f6c350e5647bfc5d
sahilsigngaze <73684596+sahil-develops@users.noreply.github.com>
2023-08-16 16:12:12 +0530

    fixes in Pricing

--- mocks/pricing.tsx
+++ mocks/pricing.tsx
@@ -14,23 +14,23 @@ export const pricing = [
         id: "1",
         title: "AI Automation",
         description:
-            "Advanced AI chatbot, priority support, analytics dashboard",
+            "AI Automation streamlines tasks and processes, optimizing efficiency and productivity across various domains.",
         // price: "9.99",
         features: [
-            "An advanced AI chatbot that can understand complex queries",
-            "An analytics dashboard to track your conversations",
-            "Priority support to solve issues quickly",
+            "AI Automation streamlines sales processes, enabling your team to focus on high-value tasks and close deals faster.",
+            "AI Automation accelerates sales cycles by optimizing workflows and eliminating bottlenecks.",
+            "AI-driven insights guide sales strategies in real time, enhancing deal dynamics and closing success.",
         ],
     },
     {
         id: "2",
         title: "AI Consulting",
-        description: "Custom AI chatbot, advanced analytics, dedicated account",
+        description: "Transform sales with expert AI Consulting for unparalleled success.",
         // price: null,
         features: [
-            "An AI chatbot that can understand your queries",
-            "Personalized recommendations based on your preferences",
-            "Ability to explore the app and its features without any cost",
+            "Leverage AI Consulting to outpace competitors with data-driven sales tactics that deliver unmatched results",
+            "AI-driven consulting uncovers deep customer insights, driving sales efforts aligned with client needs",
+            "AI Consulting identifies revenue opportunities, fine-tuning sales strategies for exponential growth.",
         ],
     },
 ];
