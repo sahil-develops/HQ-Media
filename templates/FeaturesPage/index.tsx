ebd69c9c9d3ae27b91dcd6c3bfbb6e41668b4069
Sahil Zambani <73684596+sahil-develops@users.noreply.github.com>
2023-12-12 10:50:37 +0530

    Final changes

--- templates/FeaturesPage/index.tsx
+++ templates/FeaturesPage/index.tsx
@@ -4,7 +4,7 @@ import Layout from "@/components/Layout";
 import Services from "@/components/Services";
 import Join from "@/components/Join";
 import Hero from "./Hero";
-import Benefits from "./Benefits/Index";
+{% comment %} import Benefits from "./Benefits/Index"; {% endcomment %}
 import Features from "./Features";
 import Community from "./Community";
 
