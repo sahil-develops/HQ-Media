8fab098dcc988b62c180b58e53590740320adf46
Sahil Zambani <73684596+sahil-develops@users.noreply.github.com>
2023-12-12 11:06:48 +0530

    final changes 3

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
-import FeaturesPage from "@/templates/FeaturesPage";
 
 const Home: NextPage = () => {
-    return <FeaturesPage />;
+    return <div>
+        <h1>Home</h1>
+        <p>Home page</p>
+    </div>;
 };
 
 export default Home;
