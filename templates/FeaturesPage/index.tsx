150f0027d04a0c1d18bcd15a0a86e067a5046138
Sahil Zambani <73684596+sahil-develops@users.noreply.github.com>
2023-12-12 11:09:18 +0530

    final changes 4

--- templates/FeaturesPage/index.tsx
+++ templates/FeaturesPage/index.tsx
@@ -1,10 +1,8 @@
 
 
 import Layout from "@/components/Layout";
-{% comment %} import Services from "@/components/Services"; {% endcomment %}
 import Join from "@/components/Join";
 import Hero from "./Hero";
-{% comment %} import Benefits from "./Benefits/Index"; {% endcomment %}
 import Features from "./Features";
 import Community from "./Community";
 
@@ -12,10 +10,8 @@ const FeaturesPage = () => {
     return (
         <Layout>
             <Hero />
-            {% comment %} <Benefits /> {% endcomment %}
             {/* <Features /> */}
             <Community />
-            {% comment %} <Services containerClassName="md:pb-10" /> {% endcomment %}
             <Join />
         </Layout>
     );
